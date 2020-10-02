c2af194686763938d5b30d1e4c6455cbf1e8b54c
zhabyans <zhafranku@gmail.com>
2020-10-02 12:44:35 +0000

    Deploying to gh-pages from  @ c18980ecddb566314ded732c622d5b9e63b91e77 🚀

---
+++ precache-manifest.c60783dada8d4d690ec5d9a502d5e31a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f48ea2566da34f34d22eadf56c422f9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf2e90db6416c45866d3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ae477a5af9fcb257e775",
+    "url": "/static/js/2.4885ca8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4885ca8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf2e90db6416c45866d3",
+    "url": "/static/js/main.f8258562.chunk.js"
+  },
+  {
+    "revision": "5722c4b6549706fce659",
+    "url": "/static/js/runtime-main.04c966ba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
